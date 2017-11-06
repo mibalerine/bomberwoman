d122c4bb210d218fa52104b3d08df6fd70ffe4e1
Francisco Machado <franm912@gmail.com>
2017-11-06 14:02:59 +0000

    server client wait

--- src/org/academiadecodigo/bomberwoman/threads/ServerThread.java
+++ src/org/academiadecodigo/bomberwoman/threads/ServerThread.java
@@ -1,11 +1,45 @@
 package org.academiadecodigo.bomberwoman.threads;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by codecadet on 06/11/17.
  */
 public class ServerThread implements Runnable {
+
+    private int numberOfPlayers;
+    private Socket[] clientConnections;
+    private ServerSocket serverSocket;
+
+    public ServerThread(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+        clientConnections = new Socket[numberOfPlayers];
+    }
+
     @Override
     public void run() {
 
+        waitClientConnections();
+    }
+
+    private void waitClientConnections() {
+
+        int numberOfConnections = 0;
+
+        while (numberOfConnections < numberOfPlayers) {
+
+            try {
+
+                serverSocket = new ServerSocket(8080);
+                clientConnections[numberOfConnections] = serverSocket.accept();
+
+            } catch (IOException e) {
+
+            }
+
+            numberOfConnections++;
+        }
     }
 }
