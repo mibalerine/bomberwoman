2ec33634b2f90a016872a183efbfadbc60bc78f7
Miro <bmorais@mail.com>
2017-11-06 11:21:41 +0000

    Terminal control

--- src/org/academiadecodigo/bomberwoman/Main.java
+++ src/org/academiadecodigo/bomberwoman/Main.java
@@ -6,6 +6,7 @@ package org.academiadecodigo.bomberwoman;
 public class Main {
 
     public static void main(String[] args) {
+
         new Game().start();
     }
 }
--- src/org/academiadecodigo/bomberwoman/Utils.java
+++ src/org/academiadecodigo/bomberwoman/Utils.java
@@ -1,5 +1,7 @@
 package org.academiadecodigo.bomberwoman;
 
+import java.io.IOException;
+
 /**
  * Created by codecadet on 06/11/17.
  */
@@ -7,16 +9,43 @@ public class Utils {
 
     public static int clamp(int value, int min, int max) {
 
-        if (value < min) {
+        if(value < min) {
 
             value = min;
         }
 
-        if (value > max) {
+        if(value > max) {
 
             value = max;
         }
 
         return value;
+    }
+
+    public static void rawMode() {
+
+        execute("/bin/sh", "-c", "stty raw </dev/tty");
+    }
+
+    public static void bufferedMode() {
+
+        execute("/bin/sh", "-c", "stty cooked </dev/tty");
+    }
+
+    public static void clearScreen() {
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private static void execute(String... cmd) {
+
+        try {
+
+            Runtime.getRuntime().exec(cmd).waitFor();
+        }
+        catch(InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
