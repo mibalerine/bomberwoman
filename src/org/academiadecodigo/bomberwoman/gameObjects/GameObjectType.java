8360dd94aa4dc886c2bd0fecf81b7f446084f486
Francisco Machado <franm912@gmail.com>
2017-11-10 12:16:12 +0000

    game object colors

--- src/org/academiadecodigo/bomberwoman/gameObjects/GameObjectType.java
+++ src/org/academiadecodigo/bomberwoman/gameObjects/GameObjectType.java
@@ -1,5 +1,6 @@
 package org.academiadecodigo.bomberwoman.gameObjects;
 
+import org.academiadecodigo.bomberwoman.ConsoleColors;
 import org.academiadecodigo.bomberwoman.Constants;
 
 /**
@@ -8,13 +9,13 @@ import org.academiadecodigo.bomberwoman.Constants;
 public enum  GameObjectType {
 
     PLAYER(Constants.PLAYER_CHAR),
-    BRICK(Constants.BRICK_CHAR),
-    WALL(Constants.WALL_CHAR);
+    BRICK(ConsoleColors.RED + Constants.BRICK_CHAR),
+    WALL(ConsoleColors.WHITE + Constants.WALL_CHAR);
 
     private String drawChar;
     GameObjectType(String drawChar) {
 
-        this.drawChar = drawChar;
+        this.drawChar = drawChar + ConsoleColors.RESET;
     }
 
     public String getDrawChar() {
